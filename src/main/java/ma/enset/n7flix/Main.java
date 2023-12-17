6a2489148eaef34b227702fc6178b3ec6359e38e
ilias-stack <pay.ilias2002@gmail.com>
2023-12-17 17:20:10 +0100

    Fonts added

--- src/main/java/ma/enset/n7flix/Main.java
+++ src/main/java/ma/enset/n7flix/Main.java
@@ -3,17 +3,22 @@ package ma.enset.n7flix;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FXML/hello-view.fxml"));
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/ma/enset/n7flix/Assets/Icon.png")).toExternalForm());
+        stage.getIcons().add(icon);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FXML/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(getClass().getResource("/ma/enset/n7flix/styles.css").toExternalForm());
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/ma/enset/n7flix/Styles/global.css")).toExternalForm());
+        stage.setTitle("N7FLIX!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ma/enset/n7flix/page_controllers/LoginController.java
+++ src/main/java/ma/enset/n7flix/page_controllers/LoginController.java
@@ -3,7 +3,7 @@ package ma.enset.n7flix.page_controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
